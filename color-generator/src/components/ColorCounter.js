9478026954d61e02bfe53567878c937be455c950
Aditya Prawira <adityaprawira96@gmail.com>
2021-02-14 11:46:54 +1100

    Added funtionality to change color base on values that are set for red, green, and blue which can be increased or decreased

--- color-generator/src/components/ColorCounter.js
+++ color-generator/src/components/ColorCounter.js
@@ -2,12 +2,12 @@ import React from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
 
 const styles = StyleSheet.create({});
-const ColorCounter = ({ color }) => {
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
     return (
         <View>
             <Text>{color}</Text>
-            <Button title={`Increase ${color}`} />
-            <Button title={`Decrease ${color}`} />
+            <Button title={`Increase ${color}`} onPress={() => onIncrease()} />
+            <Button title={`Decrease ${color}`} onPress={() => onDecrease()} />
         </View>
     );
 };
